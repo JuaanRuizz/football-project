ef06408792f40f0c706669c232d24bcd8a4aca29
Juan <justjuanking@gmail.com>
2024-03-17 22:13:45 -0500

    navmenu done

--- src/Components/NavMenu/NavMenu.tsx
+++ src/Components/NavMenu/NavMenu.tsx
@@ -6,6 +6,19 @@ import Home from '../Home/Home';
 import Teams from './Teams';
 import Players from './Players';
 import Logo from '../../Assets/Logo-LB3.png';
+import Aguilas from '../../Assets/logo-aguilas.png'
+import America from '../../Assets/logo-america.png'
+import Cali from '../../Assets/logo-cali.png'
+import Junior from '../../Assets/logo-junior.png'
+import Medellin from '../../Assets/logo-medellin.png'
+import Millonarios from '../../Assets/logo-millonarios.png'
+import Nacional from '../../Assets/logo-nacional.png'
+import Pasto from '../../Assets/logo-pasto.png'
+import Santafe from '../../Assets/logo-santafe.png'
+import Tolima from '../../Assets/logo-tolima.png'
+import Caldas from '../../Assets/logo-caldas.png'
+import Balon from '../../Assets/balon-esquinas.png'
+import Trofeo from '../../Assets/Tofeo-Liga.png'
 
 const NavMenu: React.FC = () => {
     return (
@@ -50,10 +63,28 @@ const NavMenu: React.FC = () => {
                         </Switch>
                     </BrowserRouter>
                 </div>
+                <div className='footer-general'>
+                    <h5>Liga BetPlay Dimayor</h5>
+                </div>
             </div>
             
             <div className='column-right'>
-                {/* Aquí deberías tener los elementos que pertenecen a la columna derecha */}
+                <div className='img-container'>
+                    <img src={Aguilas} alt="aguilas"/>
+                    <img src={America} alt="america" />
+                    <img src={Cali} alt="cali" className='cali-img'/>
+                    <img src={Junior} alt="junior" />
+                    <img src={Trofeo} alt="trofeo"  className='trofeo-img'/>
+                    <img src={Millonarios} alt="millonarios" />
+                    <img src={Nacional} alt="nacional" className='nacional-img'/>
+                    <img src={Pasto} alt="pasto" className='pasto-img'/>
+                    <img src={Santafe} alt="santafe" className='santafe-img'/>
+                    <img src={Tolima} alt="tolima"  className='tolima-img'/>
+                    <img src={Medellin} alt="medellin" className='medellin-img'/>
+                    <img src={Caldas} alt="caldas" className='caldas-img'/>
+                    {/* <img src={Balon} alt="balon-1" />
+                    <img src={Balon} alt="balon-2" /> */}
+                </div>
             </div>
         </nav>
     );
